90922d57e5dc9ecc8eb6b3ebed732288c9f1a114
TheRABU <mdfazlerabbi114@gmail.com>
2025-05-31 23:31:52 +0600

    fixed the cloudinary api_key not found error

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
-import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import express from "express";
 import cors from "cors";
-
-import bodyParser from "body-parser";
-const app = express();
-
 import imageGenerationRouter from "./routes/imageGenerationRouter.js";
 import jwtRouter from "../src/routes/jwtRouter.js";
 import userRouter from "./routes/userRouter.js";
+import bodyParser from "body-parser";
+
+const app = express();
 
 // body parser configuration
 app.use(bodyParser.json());
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,6 @@
 // const cloudinary = require("cloudinary").v2;
+import dotenv from "dotenv";
+dotenv.config();
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
@@ -7,19 +9,18 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// console.log({
-//   cloudName: `${process.env.CLOUDINARY_CLOUD_NAME}`,
-//   apiKey: process.env.CLOUDINARY_API_KEY,
-//   apiSecret: process.env.CLOUDINARY_API_SECRET,
+// console.log("Cloudinary env vars:", {
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET ? "set" : "not set",
 // });
-
 export const uploadImageToCloudinary = async (base64Image) => {
   try {
     const result = await cloudinary.uploader.upload(base64Image, {
       folder: "football-memes",
       resource_type: "image",
     });
-    console.log("Image uploaded to Cloudinary:", result.secure_url);
+    // console.log("Image uploaded to Cloudinary:", result.secure_url);
     return result;
   } catch (error) {
     console.error("Cloudinary upload error:", error);
